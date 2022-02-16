e49164a0d4749ef7616a545e26f8e098aca5aa83
Aurynn Shaw <self@aurynn.com>
2022-02-17 11:05:30 +1300

    Better systemd unit detection

--- dependencies/system_systemd.sh
+++ dependencies/system_systemd.sh
@@ -5,16 +5,16 @@ system.systemd.enable() {
 
   __babashka_log "${FUNCNAME[0]} $_unit"
   # check if the unit even exists; if it doesn't this makes no sense
-  if ! systemctl list-unit-files | grep "$_unit" > /dev/null ; then
+  if systemctl is-enabled "$_unit" | grep -q "No such file or directory" > /dev/null ; then
     __babashka_fail "${FUNCNAME[0]}: Unit $_unit not installed"
   fi
 
   is_met() {
     # how do we check if a systemd service is enabled?
-    systemctl list-unit-files | grep "$_unit" | grep enabled
+    systemctl is-enabled "$_unit" | grep -q "enabled"
   }
   meet() {
-    $__babashka_sudo systemctl enable $_unit
+    $__babashka_sudo systemctl enable "$_unit" 2>&1 > /dev/null;
   }
   process
 }
@@ -24,16 +24,16 @@ system.systemd.disable() {
   __babashka_log "${FUNCNAME[0]} $_unit"
 
   # check if the unit even exists; if it doesn't this makes no sense
-  if ! systemctl list-unit-files | grep "$_unit" > /dev/null ; then
+  if systemctl is-enabled "$_unit" | grep -q "No such file or directory" > /dev/null ; then
     __babashka_fail "${FUNCNAME[0]}: Unit $_unit not installed"
   fi
 
   is_met() {
     # how do we check if a systemd service is enabled?
-    systemctl list-unit-files | grep "$_unit" | grep disabled
+    systemctl is-enabled "$_unit" | grep -q "disabled"
   }
   meet() {
-    $__babashka_sudo systemctl disable $_unit
+    $__babashka_sudo systemctl disable $_unit 2>&1 > /dev/null;
   }
   process
 }
