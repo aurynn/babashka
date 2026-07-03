147f2c36b2a66cd3dc1be82a00f344b7bfee59af
Aurynn Shaw <self@aurynn.com>
2026-07-03 16:42:16 +1200

    Update `system_file_symlink.sh` to newer standards Update variable handling to proper double-quoted standards.

--- dependencies/system_file_symlink.sh
+++ dependencies/system_file_symlink.sh
@@ -3,14 +3,15 @@
 # Do modes make sense for this?
 
 function system.file.symlink() {
-  local _source=$1; shift
-  local _dest=$1; shift
+  local _source _dest
+  source="$1"
+  dest="$2"
 
   __babashka_log "== system.file.symlink ${_source} ${_dest}"
 
   # Source and dest should be absolute paths, not relative
-  for _path in $_source $_dest; do
-    case $_path in
+  for _path in "$_source" "$_dest"; do
+    case "$_path" in
       (/*)
         pathchk -- "$_path";;
       (*)
@@ -25,26 +26,26 @@ function system.file.symlink() {
   }
 
   is_met() {
-    [[ -e ${_dest} && -L ${_dest} ]] && \
-      [[ $($__babashka_sudo readlink ${_dest}) == ${_source} ]]
+    [[ -e "${_dest}" && -L "${_dest}" ]] && \
+      [[ $(readlink "${_dest}") == "${_source}" ]]
   }
   meet() {
-    if ! [[ -e ${_source} ]]; then
+    if ! [[ -e "${_source}" ]]; then
       __babashka_fail "${_source} does not exist"
     fi
-    if [[ -e ${_dest} ]]; then
+    if [[ -e "${_dest}" ]]; then
       # the destination is NOT a link, so, we should error in this case
       # and let the user manage it directly
-      if ! [[ -L ${_dest} ]]; then
-        if [[ __babashka_force != "yes" ]]; then
+      if ! [[ -L "${_dest}" ]]; then
+        if [[ "$__babashka_force" != "yes" ]]; then
           __babashka_fail "${_dest} exists and is not a symlink; use -f"
           return 1
         fi
         __babashka_log "WARN: ${_dest} exists and is not a symlink; unlinking"
       fi
-      $__babashka_sudo rm ${_dest}
+      rm "${_dest}"
     fi
-    $__babashka_sudo ln -s ${_source} ${_dest}
+    ln -s "${_source}" "${_dest}"
   }
   process
 }
